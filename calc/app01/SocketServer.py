4e54409f96d25952ef9258086010d551c44b459f
zrhan <2829442630@qq.com>
2022-11-06 13:46:12 +0800

    websocket测试8

--- calc/app01/SocketServer.py
+++ calc/app01/SocketServer.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 
 import asyncio
+import time
 
 import websockets
 
 
+async def msgSender(websocket):
+    for i in range(0, 100):
+        print("sended "+str(i))
+        websocket.send(str(i))
+        time.sleep(1)
+
+
+
+
+
+
 async def handler(websocket):
+    asyncio.run(msgSender())
     async for message in websocket:
         print("收到信息："+message)
-    async for i in range(0, 100):
-        websocket.send(str(i))
+
 
 
 
