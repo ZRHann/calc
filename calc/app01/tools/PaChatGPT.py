ca1e9a34ced28d043137d35402a09aeb03fc9b11
zrhan <2829442630@qq.com>
2022-12-08 20:35:54 +0800

    测试ChatGPT爬虫7

--- calc/app01/tools/PaChatGPT.py
+++ calc/app01/tools/PaChatGPT.py
@@ -8,12 +8,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-option = webdriver.FirefoxOptions()
-option.add_argument('--headless')
-option.add_argument('--disable-gpu')
-profile_dir = r'./app01/MyData/a2s3528m.default-release'
-conf_profile = webdriver.FirefoxProfile(profile_dir)
-driver = webdriver.Firefox(conf_profile)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('start-maximized')
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument('--disable-browser-side-navigation')
+chrome_options.add_argument('enable-automation')
+chrome_options.add_argument('--disable-infobars')
+chrome_options.add_argument('enable-features=NetworkServiceInProcess')
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://chat.openai.com/")
 print(driver.page_source)
 
