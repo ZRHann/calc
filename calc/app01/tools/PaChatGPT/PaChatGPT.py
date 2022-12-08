438e2115afdf985d53f55ce79d993de6b94846ac
zrhan <2829442630@qq.com>
2022-12-08 23:26:27 +0800

    ChatGPT爬虫测试13

--- calc/app01/tools/PaChatGPT/PaChatGPT.py
+++ calc/app01/tools/PaChatGPT/PaChatGPT.py
@@ -12,7 +12,7 @@ import time
 
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('start-maximized')
