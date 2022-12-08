40c17d8e898562f0ce715cacd6751921727d7c7f
zrhan <2829442630@qq.com>
2022-12-08 23:24:45 +0800

    ChatGPT爬虫测试12

--- calc/app01/tools/PaChatGPT/PaChatGPT.py
+++ calc/app01/tools/PaChatGPT/PaChatGPT.py
@@ -10,12 +10,9 @@ from selenium.webdriver.support import expected_conditions as EC
 import json
 import time
 
-CookieList = []
-with open('./app01/tools/PaChatGPT/cookies.txt', 'r') as f:
-    CookieList = json.loads(f.read())
-print(CookieList)
+
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('start-maximized')
@@ -24,19 +21,15 @@ chrome_options.add_argument('--disable-browser-side-navigation')
 chrome_options.add_argument('enable-automation')
 chrome_options.add_argument('--disable-infobars')
 chrome_options.add_argument('enable-features=NetworkServiceInProcess')
+chrome_options.add_argument(r'--user-data-dir='+os.getcwd()+"/app01/tools/PaChatGPT/userdata/data1")
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://chat.openai.com/chat")
-print(driver.get_cookies())
-driver.delete_all_cookies()
-for cookie in CookieList:
-    print(cookie)
-    driver.add_cookie(cookie)
-driver.refresh()
 print(driver.page_source)
 
 WebDriverWait(driver, 1000).until(
     EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div[1]/main/div[2]/form/div/div[2]/button'))
 )
+print('debug1')
 textarea1 = driver.find_element("xpath", '/html/body/div/div/div[1]/main/div[2]/form/div/div[2]/textarea')
 btn1 = driver.find_element("xpath", '/html/body/div/div/div[1]/main/div[2]/form/div/div[2]/button')
 
@@ -49,13 +42,6 @@ def SendAndReceive(msg):
     )
     BotAnswerP = driver.find_elements("xpath", '/html//p')[-2]
     print(BotAnswerP.text)
-
-
-def GetLoginCookie():
-    cookie = ""
-    with open('cookies.txt', 'r') as f:
-        cookie = f.read()
-    print(cookie)
 
 
 SendAndReceive("3543")
