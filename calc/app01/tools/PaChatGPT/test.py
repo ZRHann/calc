825337b0f6e7c3b62067167a2bf53bf885ced14c
zrhan <2829442630@qq.com>
2022-12-10 14:47:20 +0800

    ChatGPT爬虫测试45

--- calc/app01/tools/PaChatGPT/test.py
+++ calc/app01/tools/PaChatGPT/test.py
@@ -1,8 +1,14 @@
-import pymysql
-db = pymysql.connect(host='localhost',
-                                  user='root',
-                                  password='zg801zrh160118.',
-                                  database='ChatGPT_OL')
-cursor = db.cursor()
-sql = """INSERT INTO conversation_log(msg_json) VALUES ()"""
-cursor.execute(sql)
+from revChatGPT.revChatGPT import Chatbot
+config = {
+            # "email": "2829442630@qq.com",
+            # "password": "zg801zrh160118.",
+            "session_token": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..qCQO7qHkYpGrT24h.B49chTdPTe2x6XBVti88A3o5KdvH8C7TO3zyOENMFB5KNZDBIo7wTR_hxAtBoq53uTcMKzlnGpTzCJ5Y_rUiKr7MSbcILVjWkCZxel2Y9QoTuyeqPuV9keRAMaWitxLXVbrc1Hf2IfnguHyhPsjyAiYRYTSxgGXNaGSEnUsS84PQnnn-JkK-XDTM53pwb6DpyxVAuVU5ernD2TPyQ1ZEn2rU66KeOfVzmSNRzskCyhRWWu65ickKTvEvioP97XrSFn9HZATKaI4z9SARy2Rgssk5o5sybnom53OXXuw--2SBEiijnviD3ACOFTqnhPsbvS605fPVTYWtEG_YXLMiz-YToILfX-cexPqlApyWjHfwajzfcq58Jw-R5psdOrrp5l4mMDtqBDOpqTjT0eW2R5wjhij_ZStHXSRvnnm4KszDUAtrJlEGdvSSU9dxbGHXRtPP1-7jpBpYOOtkR1yeoKnrVser1bZ2_xB-W56sEpXncGLc0RJVQK70drlS5aYRn2juAVa5f9nKM6vO8B8rPYr2tVMpzVVzPlxNUrIJ5eirz8Mhh2vIkJAYmn8gXFfH0CMyJpj-lvfdwVCPC_cRN3djUoyowKX2XIN5yXOfGT8pPx4MtlDAQijOPsc5cnIJAidRLf_Hc_isWrDH4CSIjXwano7Jte1n0f7Eq4DewHdsW-lh2RR0D_bo5VmPKBkOTGUuuRaX9tpO_tLFJqIPVbZO-cNjBfQ5Red4hM3GwVBTQAidz4g0D-sVF7Y-OOUgu8K7JnjosFDz4x_Nj00PjzcLDypahFB0Sa10URHp7XDf1SwnQAH3YhutGWoVVPmmrPhC6XEpQUTf6qT2l0vw3FKVRa6RAwvY5v7x0r7fXI1gyfAvEC2k01kSbr7eQiDKIzuYQcOD81O5_SWF-gKxFKxLKIOCYAg68I4kebDyuY0-DBef1wiA5B6uO5piLtyQ3HNWF53sMM7DWPiX4Oh-IOxt8fx8GTbHz8xzzo119IAP4uPn2qRdprxJPDaWLucoOYR4a9AKdl8Hk9DdrfBxdF8llULNapv6zjnY2qgcAXdtEmi8nTw63EnVgerSc0eG2TJOuMiQOxIt5yVDpuPJ0zwCmXz5-NCBElHi9uqx3NyyAonybRygMfu9OaEM40LahqqNqXKPJttsc7jzgymGQNfFiRYx5wIUgz0uvqdiKQTva-88bffknNpfLWm9q9K7PDz-9tPuH4lYA8BeiI77Jo7MMfdriLfImqHs6bYbnLKTiQuac_vVNwVSuvn3bCEr33xppir0mINgAiF3jQoPh1Kfth2QMdLq8ED7CbchN_R3dgzb3b29KDOLwYvVTRDi6VKiDo-JrmLedV6PDqKhdmdXp7hLY7UefK8WHd0g031KtvyVs294pRCWADzQeXiryuijysUnj1maRlBMR12M2ro8nac7zzLSNGJ82064tt99nHqdc5vxdTxaFeCe82o_BskFrzjviAvrAkq5Pjt33OJJw-kM0uab2mgljHXyF9KWauDRjfktDlJWJrLwkLf8zowafv9A_cz-wYbwsyJl9YmlXoDyCSt2VezvQ775E61n9mk12DjjhQxD_bjG0rSEksk9PtdxxvczMo741X5lAajSVA-z5pdA7QjWW2vLJxZWW8lHEozigoQff6PV2p2d9QOmC0lqpBv-McSHv6fqpoiawVQo57yBm0Q5sCR0PkyNtdiLlOAY9LaUL_A6i05nhiSFeD4-eKiZG0ubrHEvjc3x9myxpPLBieGMbUfnaD7lCDV_N_F3Y633PhqDmTDTrS6VDyh1--EWw-h8Ujx7rjmMIx_3XMA15R3UWuVwX7NhfTJOASWUXnGbGdtqWBnJySz5ZyJ8tgFMIgSxIV6fva3Q1TTcEAUE7NrS4T3yJG_tuH6bCVOgo17VSRnl2TYyGhdkzo7IMMw-ruWCHfADyVoAnxzO5meXJx4uiGoQUTv4_gzOSaWsWXH3f57fUMbw1yH_L-6lb3sUnQC-Sw7d6p7RZlJ1xbUoQD3hd9IHdvqvxt8TsOGpkAUvUR-bgdrz1Zq2jusbdpMwe_hUrhKmOzOJGkm-3jDtv94zMO1MRYfgZMU9OwjKDyfKY8TuZtmLwtz1rhINrKW9p0cf7qWyXJbLjPyanJV9V8WrTjf_dWLg2O3hGRXAoIVgDPzsIU3f6vS1Q0wGpbKsTvSMrITreq_f7Ofmt9KI0JhMPCMOyZrNWDcfE9tNbdyPgFqNEJ3ISW3-NoI.vj8COqcHM0Ao-eA4-a_rfA",
+            # Deprecated. Use only if you encounter captcha with email/password
+            # "proxy": "<HTTP/HTTPS_PROXY>"
+        }
+chatbot = Chatbot(config, conversation_id=None)
+print("connected")
+response = chatbot.get_chat_response("Hello world", output="stream")
+for res in response:
+    print(res["message"])
+
